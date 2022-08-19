9e968f54212ef70802818a37dc51783e1fc07f0a
KSY9808 <kimsy9808@naver.com>
2022-08-19 18:02:45 +0900

    220819 commit

--- app/src/main/java/com/corporation8793/festival/LoginActivity.java
+++ app/src/main/java/com/corporation8793/festival/LoginActivity.java
@@ -1,14 +1,70 @@
 package com.corporation8793.festival;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView joinText;
+    String id, pw;
+    EditText editText1, editText2;
+    TextView textView1;
+    Button move_MainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // 회원가입 색상 변경
+        joinText = findViewById(R.id.joinText);
+        String textData = joinText.getText().toString();
+        SpannableStringBuilder builder = new SpannableStringBuilder(textData);
+
+        String word = "회원가입";
+        int start = textData.indexOf(word);
+        int end = start + word.length();
+
+        builder.setSpan(new ForegroundColorSpan(Color.parseColor("#ad9cfd")), start, end, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        joinText.setText(builder);
+
+        // 로그인시 MainActivity 이동
+        id = "wisi8793";
+        pw = "1234";
+        editText1 = findViewById(R.id.rectangle1);
+        editText2 = findViewById(R.id.rectangle2);
+        textView1 = findViewById(R.id.errorText);
+
+        move_MainActivity = findViewById(R.id.loginButton);
+        move_MainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user_id = editText1.getText().toString();
+                String user_pw = editText2.getText().toString();
+
+                if(user_id.equals("") || user_pw.equals("")) {
+                    textView1.setText("아이디와 비밀번호를 입력하세요.");
+                } else if(!user_id.equals(id) || !user_pw.equals(pw)) {
+                    textView1.setText("아이디와 비밀번호가 일치하지 않습니다.");
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
+
 }
