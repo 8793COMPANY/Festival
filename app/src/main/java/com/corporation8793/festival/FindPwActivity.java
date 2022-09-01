70a47133c59d451d8ff29cdf43b224046d3d3f22
KSY9808 <kimsy9808@naver.com>
2022-09-01 17:59:09 +0900

    220901 commit

--- app/src/main/java/com/corporation8793/festival/FindPwActivity.java
+++ app/src/main/java/com/corporation8793/festival/FindPwActivity.java
@@ -2,13 +2,39 @@ package com.corporation8793.festival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindPwActivity extends AppCompatActivity {
+
+    Spinner choiceQuestion;
+    ArrayAdapter<CharSequence> choiceQuestion_adapter;
+    Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_pw);
+
+        choiceQuestion = findViewById(R.id.choiceQuestion);
+        nextButton = findViewById(R.id.nextButton);
+
+        choiceQuestion_adapter = ArrayAdapter.createFromResource(this, R.array.question_text, android.R.layout.simple_spinner_item);
+        choiceQuestion_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        choiceQuestion.setAdapter(choiceQuestion_adapter);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FindResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
