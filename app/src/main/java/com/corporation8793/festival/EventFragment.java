91314e82b62dce24cb4548c749688ed0ed221aca
KSY9808 <kimsy9808@naver.com>
2022-09-08 16:40:10 +0900

    220908 commit

--- app/src/main/java/com/corporation8793/festival/EventFragment.java
+++ app/src/main/java/com/corporation8793/festival/EventFragment.java
@@ -3,17 +3,65 @@ package com.corporation8793.festival;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class EventFragment extends Fragment {
+
+    RecyclerView recyclerView2;
+    RecyclerAdapter recyclerAdapter2;
+    Spinner choiceMonth2, choiceArea2;
+    ArrayAdapter<CharSequence> choiceMonth_adapter2, choiceArea_adapter2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+
+        recyclerView2 = view.findViewById(R.id.recyclerView2);
+        choiceMonth2 = view.findViewById(R.id.choiceMonth2);
+        choiceArea2 = view.findViewById(R.id.choiceArea2);
+
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView2.setLayoutManager(layoutManager2);
+        recyclerAdapter2 = new RecyclerAdapter(getActivity());
+
+        recyclerAdapter2.addItem(new Festival("2022.09.01~09.12", "광주 맥주 축제", R.drawable.festival_image_1));
+        recyclerAdapter2.addItem(new Festival("2022.09.14~09.20", "가을 꽃 축제", R.drawable.festival_image_2));
+        recyclerAdapter2.addItem(new Festival("2022.09.31", "3", R.drawable.festival_image_3));
+        recyclerAdapter2.addItem(new Festival("4", "4", R.drawable.festival_image_1));
+        recyclerAdapter2.addItem(new Festival("5", "5", R.drawable.festival_image_2));
+
+        recyclerView2.setAdapter(recyclerAdapter2);
+
+        choiceMonth_adapter2 = ArrayAdapter.createFromResource(getActivity(), R.array.month_array, R.layout.spinner_item);
+        choiceMonth_adapter2.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        choiceMonth2.setAdapter(choiceMonth_adapter2);
+
+        choiceMonth2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //((TextView));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        choiceArea_adapter2 = ArrayAdapter.createFromResource(getActivity(), R.array.area_array, R.layout.spinner_item);
+        choiceArea_adapter2.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        choiceArea2.setAdapter(choiceArea_adapter2);
+
+        return view;
     }
 }
--- app/src/main/java/com/corporation8793/festival/MyPageFragment.java
+++ app/src/main/java/com/corporation8793/festival/MyPageFragment.java
@@ -1,5 +1,6 @@
 package com.corporation8793.festival;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MyPageFragment extends Fragment {
+
+    TextView logoutText;
+    ArrayAdapter<CharSequence> myPageAdapter;
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_page, container, false);
+
+        logoutText = view.findViewById(R.id.logoutText);
+        logoutText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        listView = view.findViewById(R.id.listView);
+        myPageAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.mypage_array, android.R.layout.simple_list_item_1);
+        listView.setAdapter(myPageAdapter);
+
+        return view;
     }
 }
